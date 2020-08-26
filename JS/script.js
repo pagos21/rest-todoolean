71df7cb36fffd2e9e0383190a2db628c56c94d9a
pagos21 <paga@localhost.yeah>
2020-08-26 16:50:27 +0200

    v 1.1 - toolTip + features

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,4 @@
 
-
-
 function init(){
   callMyAPI();
   userInputF();
@@ -33,30 +31,45 @@ function printMyAPI(data){
   }
 }
 
+// In caso si usi il classico bottone
 function userInputF(){
   var btn = $('#btn');
   btn.click(function(){
     var uInput = $("#uInput");
     var utxt = uInput.val();
-
-    $.ajax({
-      url: `http://157.230.17.132:3020/todos`,
-      method: 'POST',
-      data: {
-        text: utxt
-      },
-      success: function(data){
-        callMyAPI();
-      },
-      error: function(err){
-        console.log("Errore dalla chiamata POST", err);
-      }
-    })
-
+    callAPI4POST(utxt);
   });
 }
 
 
+// Nel caso si usi l'invio
+function enterkey(){
+  if (event.keyCode === 13){//13 equivale a ENTER
+    var uInput = $("#uInput");
+    var utxt = uInput.val();
+    callAPI4POST(utxt);
+  }
+}
+
+
+// Generalizzo l'ajax(POST) per non riscriverla 2 volte
+function callAPI4POST(arg4API){
+  $.ajax({
+    url: `http://157.230.17.132:3020/todos`,
+    method: 'POST',
+    data: {
+      text: arg4API
+    },
+    success: function(data){
+      callMyAPI();
+    },
+    error: function(err){
+      console.log("Errore dalla chiamata POST", err);
+    }
+  })
+}
+
+// Per eliminare dal server API
 function delFromList(){
   $(document).on("click", ".listSingle", function(){
     var element = $(this);
